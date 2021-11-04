cbf9d951c60a1885f0af0cf5b7b2e5cc7c77915f
hanapk <pknissar@gmail.com>
2021-11-04 06:59:31 +0000

    URL reset

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/hanapk/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('Hana PK')
-            node / gitConfigEmail('pknissar@gmail.com')
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
